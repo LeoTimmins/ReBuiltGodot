8817773625975783edc51c62cc8dbe59bd52983f
Leo Timmins <70576815+EvilAndStuff@users.noreply.github.com>
2022-03-11 15:08:34 +0800

    Wheel Rotation (Actual because lucas did it)

--- Scripts/PlayerKinetic.gd
+++ Scripts/PlayerKinetic.gd
@@ -22,6 +22,14 @@ onready var wheel = get_node(wheel_path)
 
 var velocity = Vector3.ZERO
 
+func normalize_angle(angle: float):
+	if angle > 2*PI:
+		return 0;
+	elif angle < 0:
+		return 2*PI
+	else:
+		return angle;
+	
 func _ready():
 	#Hide Mouse
 	Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED)
@@ -31,14 +39,17 @@ func _input(event):
 		var mouse_speed = event.get_relative();
 		mouse_speed.normalized();
 		camera_pivot.rotation.y -= mouse_speed.x / 200;
+		camera_pivot.rotation.y = normalize_angle(camera_pivot.rotation.y);
 		camera_pivot.rotation.x += mouse_speed.y / 200;
 		camera_pivot.rotation.x = max(min(camera_pivot.rotation.x, 0.5), -0.5);
 	if event is InputEventKey:
 		if Input.is_action_just_pressed("Debug_GiveMouse"):
 			Input.set_mouse_mode(Input.MOUSE_MODE_VISIBLE);
 
-var interpo_time = 0.0
+var interpo_time = 0.0;
 var rotating = false;
+
+var wheel_wobble_right = true;
 
 func _physics_process(delta):	
 	# Debugging
@@ -54,12 +65,22 @@ func _physics_process(delta):
 	var input_vector = Input.get_vector("ui_left", "ui_right", "ui_down", "ui_up");
 	input_vector.normalized();
 	move_direction = input_vector.x * right + input_vector.y * forward;
+	move_direction.y = 0;
 	wheel.rotation.x += input_vector.y;
+	wheel.rotation.x = normalize_angle(wheel.rotation.x);
+	if wheel_wobble_right:
+		wheel.rotation.z += input_vector.y / 20000;
+		if wheel.rotation.z >= PI/4:
+			wheel_wobble_right = false;
+	else:
+		wheel.rotation.z -= input_vector.y / 20000;
+		if wheel.rotation.z <= -PI/4:
+			wheel_wobble_right = true;
+	
 	# Detect Inputs and Vector math
 	if Input.is_action_just_pressed("ui_left") || Input.is_action_just_pressed("ui_right") || Input.is_action_just_pressed("ui_up") || Input.is_action_just_pressed("ui_down"):
 		rotating = true;
 		
-
 	if Input.is_action_pressed("ui_right") ||  Input.is_action_pressed("ui_left") ||  Input.is_action_pressed("ui_up") ||  Input.is_action_pressed("ui_down"):
 		if rotating == false:
 			mesh.rotation.y = camera_pivot.rotation.y;
@@ -71,14 +92,14 @@ func _physics_process(delta):
 			interpo_time = 0.0;
 		else:
 			mesh.rotation = mesh.rotation.linear_interpolate(Vector3(mesh.rotation.x, camera_pivot.rotation.y, mesh.rotation.z), interpo_time);
+			collision.rotation = mesh.rotation;
 			interpo_time += delta * 0.8;
 			if interpo_time > 1.0:
 				interpo_time = 1.0;
 
 	# Jumping
 	if Input.is_action_just_pressed("SpaceBar") && is_on_floor():
-		move_direction.y = 10;
-		
+		move_direction.y = 8;
 		
 	#Apply movement and velocity calculations
 	velocity.x = move_direction.x * speed
